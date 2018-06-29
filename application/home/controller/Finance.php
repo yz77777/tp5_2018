26b89390d4b3a8cb09e866d34151e02e350ce488
yz <996694995@qq.com>
2018-06-29 18:42:10 +0800

    new add

---
+++ application/home/controller/Finance.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\home\controller;
+use app\home\logic;
+use think\Config;
+use think\controller;
+class Finance extends controller
+{
+	public function index()
+	{
+
+		$pay_method = Config::get('common.pay_method');
+		$income_method = Config::get('common.income_method');
+		$consume_type = Config::get('common.consume_type');
+//dump($pay_method);die;
+
+
+		$this->assign('income_method',$income_method);
+		$this->assign('consume_type',$consume_type);
+		$this->assign('pay_method',$pay_method);
+
+		return view();
+	}
+}
---
+++ application/home/logic/FinanceLogic.php
@@ -0,0 +1,7 @@
+<?php
+namespace app\home\logic;
+use think\model;
+class FinanceLogic
+{
+
+}
