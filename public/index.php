61a718615a01e5a7fbb76c41b792bd327eae1828
mac_yz <wyz_9966@163.com>
2018-08-02 16:49:09 +0800

    默认模块

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 define('CONF_PATH', __DIR__. '/../application/config/');
-
+define('BIND_MODULE','home');
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
