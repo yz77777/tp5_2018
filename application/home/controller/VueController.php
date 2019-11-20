03d4c17764cfbb4524399d732bbebc5917a8865f
mac_yz <wyz_9966@163.com>
2019-11-20 19:21:52 +0800

    vue

---
+++ application/home/controller/VueController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 大漠孤颜值
+ * Date: 2019/11/20
+ * Time: 下午4:07
+ */
+namespace app\home\controller;
+use think\Controller;
+
+class VueController extends Controller {
+
+	public function index() {
+
+		return view();
+	}
+}
