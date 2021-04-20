4997da9953ff2fc579e09242c23ab20a06357127
mac_yz <wyz_9966@163.com>
2021-04-20 11:25:23 +0800

    db

--- application/config/database.php
+++ application/config/database.php
@@ -4,13 +4,13 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'zkt_weekend',
+    'database'        => 'y',
     // 用户名
     'username'        => 'root',
     // 密码
     'password'        => '',
     // 端口
-    'hostport'        => '33061',
+    'hostport'        => '3306',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
